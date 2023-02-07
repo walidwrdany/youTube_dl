f7f2f087b6d15c4b7789ee631a37a8a86a166b12
Walid Wrdany <computek246@gmail.com>
2023-02-07 23:41:29 +0200

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,66 @@
+@echo off
+
+
+:: BatchGotAdmin
+:-------------------------------------
+
+REM  --> Check for permissions
+IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+	>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+	>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/k ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+
+SETLOCAL EnableDelayedExpansion
+
+
+choco -? >nul 2>&1 || (
+    echo Installing Chocolatey...
+	powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+)
+
+choco install youtube-dl yt-dlp ffmpeg atomicparsley -y --force
+
+
+REM ===============================================================================
+
+>nul reg delete "HKEY_CLASSES_ROOT\Directory\background\shell\youtube" /F
+reg add "HKEY_CLASSES_ROOT\Directory\background\shell\youtube" /D "youtube-dl" /F
+reg add "HKEY_CLASSES_ROOT\Directory\background\shell\youtube" /V "Icon" /D "C:\Windows\system32\cmd.exe,0" /F
+::reg add "HKEY_CLASSES_ROOT\Directory\background\shell\youtube" /v Extended /f
+reg add "HKEY_CLASSES_ROOT\Directory\background\shell\youtube\command" /D "\"%~dp0\y.bat\" \"%%v\"" /F
+
+REM ===============================================================================
+
+
+echo.
+echo.
+pause
+exit
+
+
+
+
+
+
---
+++ y.bat
@@ -0,0 +1,213 @@
+@echo off
+
+setlocal EnableDelayedExpansion
+chcp 65001
+
+
+REM ( ͡° ͜ʖ ͡°)
+
+
+:------------------------------------------------------------
+
+
+	echo.Loading...
+	
+	for /f "tokens=*" %%a in ('yt-dlp.exe --version') do ( set "youtube_version=%%a" )
+	for /f "tokens=*" %%a in ('ffmpeg.exe -version') do ( set "ffmpeg_version=%%a" )
+	
+	
+	set "current_directory=%~1"
+	set "config_txt=%tmp%\yt-dlp-config.txt"
+	set "format_txt=%tmp%\yt-dlp-formats.txt"
+	set "archive_txt=%~dp0\archive.txt"
+
+:------------------------------------------------------------
+
+
+:Start
+
+	
+	call :Desplay
+
+	set /p "url=Link :"
+	set /p "index=Index :"
+	echo.
+	echo.
+	
+	if not defined index (
+		set index=1
+	)
+	
+	
+	yt-dlp.exe --list-formats --playlist-items %index% %url% > %format_txt%
+	yt-dlp.exe -v --list-formats --playlist-items %index% %url%
+	
+
+	findstr /i /r /c:"playlist" "%format_txt%" >nul && (
+		set isPlayList=Yes
+	) || (
+		set isPlayList=No
+	)
+
+:------------------------------------------------------------
+
+
+:Loop
+
+
+	echo.
+	echo.
+	set /p "formatCode=Format Code :"
+	set codeStatus=
+
+
+	findstr /i /r /c:"^%formatCode%\>" "%format_txt%" >nul && (
+		if not defined codeStatus findstr /I /R /C:"^%formatCode% .*audio" "%format_txt%" >nul && (set codeStatus=AUDIO ONLY)
+		if not defined codeStatus findstr /I /R /C:"^%formatCode% .*video" "%format_txt%" >nul && (set codeStatus=VIDEO ONLY)
+		if not defined codeStatus findstr /I /R /C:"^%formatCode% .*best" "%format_txt%" >nul && (set codeStatus=BEST OPTION)
+	) || (
+		set codeStatus=CUSTOM FORMAT
+	)
+	
+	
+	if not defined codeStatus (
+		
+		set "Mess=You choice '%formatCode%' | Are you Sure"
+		
+	) else (
+		
+		set "Mess=You choice '%formatCode%' and this is | %codeStatus% | Are you Sure"
+		
+	)
+
+
+	echo.
+	choice /C YN /M "%Mess%"
+	echo.
+	if %errorlevel%==1 goto :Download
+
+	goto :Loop
+
+
+:------------------------------------------------------------
+
+
+:Download
+
+	
+	if exist "%config_txt%" del "%config_txt%"
+	
+	set baseOut=%current_directory%\%%(extractor)s
+	set download_folder_audio=%baseOut%\Audio\
+	set download_folder_videos=%baseOut%\Video\
+	set playlist=%%(channel)s\%%(playlist)s
+	set output=%%(playlist_index^|)s%%(playlist_index^& - ^|)s%%(title)s %%(id)s.%%(ext)s
+
+	(
+
+		echo.# Lines starting with # are comments
+		echo.# https://github.com/yt-dlp/yt-dlp
+		echo.
+		echo.
+		echo.--console-title
+		echo.--embed-thumbnail
+		echo.--no-overwrites
+		echo.--yes-playlist
+		echo.--download-archive "%archive_txt%"
+		echo.--no-embed-info-json
+		echo.
+		echo.
+
+
+		if defined formatCode (
+			echo.--format "%formatCode%/bestvideo[ext=mp4][height<=?720]+bestaudio[ext=m4a]/best[ext=mp4][height<=?720]/best"
+		) else (
+			echo.--format "bestvideo[ext=mp4][height<=?720]+bestaudio[ext=m4a]/best[ext=mp4][height<=?720]/best"
+		)
+
+		echo.%codeStatus% | findstr /I /R /C:"AUDIO" >nul && (
+
+			echo.--metadata-from-title "%%(artist)s - %%(title)s"
+			echo.--extract-audio
+			echo.--audio-format mp3
+			echo.--audio-quality 0
+
+			if "%isPlayList%"=="Yes" (
+				echo.--output "%download_folder_audio%\%playlist%\%output%"
+			) else (
+				echo.--output "%download_folder_audio%\%output%"
+			)
+
+		) || (
+
+			echo.--embed-metadata
+			echo.--merge-output-format mp4
+			echo.--xattrs
+			echo.--embed-subs
+			echo.--write-description
+
+			if "%isPlayList%"=="Yes" (
+				echo.--output "%download_folder_videos%\%playlist%\%output%"
+			) else (
+				echo.--output "%download_folder_videos%\%output%"
+			)
+
+		)
+
+	) >> "%config_txt%"
+
+
+	
+	
+	yt-dlp.exe -v --config-location "%config_txt%" %url%
+	
+	
+
+:------------------------------------------------------------
+
+
+:ReStart
+	echo.
+	echo.
+	pause
+	goto :Start
+	
+
+:------------------------------------------------------------
+
+
+:Desplay
+
+	set "Modified=%~t0"
+	set Version=!Modified:~6,4!.!Modified:~0,2!.!Modified:~3,2!
+
+	cls
+	echo.
+	echo.           youtube-dl is a command-line program to download videos from youTube and a few more sites
+	echo.                       ffmpeg version %ffmpeg_version%
+	echo.                          yt-dlp version %youtube_version%   updated !Version!
+	echo.
+	echo.
+	goto :eof
+
+
+:------------------------------------------------------------
+
+
+:Exit
+	echo.
+	pause
+	exit
+
+
+:------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
